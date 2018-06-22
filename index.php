657daf7e96fde4588adc73185e4c875219d2cb07
Devon Thomassen <36296870+w1-ThomassenDevon@users.noreply.github.com>
2018-06-22 17:51:08 +0200

    index.php
    test 'template' van html code

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+    Opdracht:       Eindproject, PhpStorm
+    Auteur:         Devon Thomassen, Steve V
+    Datum + tijd:   20-6-2018 20:21
+    Bestandsnaam:   index.php
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>index.php</title>
+	<link rel="stylesheet" href="StyleSheet.css" type="text/css">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TimelineMax.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js"></script>
+	<script src="script.js" defer></script>
+</head>
+<body>
+<div id="home">
+	<header>
+		<img src="https://www.mojo.nl/images/mojo-logo-triangle.svg">
+		<ul>
+			<li><a href="#home">Home</a></li>
+			<li><a href="#">Evenementen</a></li>
+			<li><a href="#">Artiesten</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Over ons</a></li>
+		</ul>
+		<section class="menu_icon">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</section>
+	</header>
+	<main>
+		<section class="arrowL">
+			<span></span>
+			<span></span>
+		</section>
+		<section class="arrowR">
+			<span></span>
+			<span></span>
+		</section>
+		<img src="https://www.mojo.nl/image/1615242/snow-patrol-promo.jpg?format=Y2CmOPulHexn3W77bt5egDe6f6UJeeVDXFnBo6XtabEb7yTSDm1Fvw%3d%3d">
+	</main>
+</div>
+</body>
+</html>
